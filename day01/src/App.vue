1df564e676ac7bd3d254bafb5b7e82499b941ba6
rushi1225 <rushibhingare25@gmail.com>
2025-02-19 00:07:04 +0530

    vuejs

--- day01/src/App.vue
+++ day01/src/App.vue
@@ -1,43 +1,18 @@
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
 </script>
 
 <template>
-  <header>
-    <h1>Hello Rushikesh Bhingare</h1>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+ 
+    <h1 class="head">Hello Rushikesh Bhingare</h1>
+  
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+h1{
+  background-color: aqua;
+  padding: 10px;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.head{
+  color: red;
 }
 </style>
